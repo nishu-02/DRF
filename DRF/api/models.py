d68974c6660febc141c987047f6c41f4e9d346ea
nishu-02 <0264nishu@gmail.com>
2025-05-13 18:11:26 +0530

    Creating the Product Class

--- DRF/api/models.py
+++ DRF/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    pass
+
+class Product(models.Models):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveInteferField()
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+
+    @property
+    def in_stock(self):
+        return self.stock > 0
+
+    def __str__(self):
+        return self.name
+        
