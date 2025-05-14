64d34f642aa2e39b9bcd9cddad8f2da62cc43a5e
nishu-02 <0264nishu@gmail.com>
2025-05-15 00:23:59 +0530

    A function based view to get a single product

--- DRF/api/urls.py
+++ DRF/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('produdcts/', views.product_list)
+    path('produdcts/', views.product_list),
+    path('products/<int:pk>/', views.product_detail),
 ]
--- DRF/api/views.py
+++ DRF/api/views.py
@@ -1,12 +1,23 @@
-from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from api.serializers import ProductSerializer
 from api.models import Product
+from rest_framework.response import Response # we pass the data
+from rest_framework.decorators import api_view # function based views
 
 # Create your views here.
 
+# so the interface we see on the broweser it is beacuse of the render classes in the DRF
+
+@api_view(['GET'])
 def product_list(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True) # Instantitate the Product serializer
-    return JsonResponse({
-        'data': serializer.data
-    })
+    return Response(serializer.data) # Performs content negotiation on its own
+
+
+@api_view(['GET'])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
