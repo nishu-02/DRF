fea04284d109eeec9223049d795c796df5d27e63
nishu-02 <0264nishu@gmail.com>
2025-05-19 00:27:12 +0530

    More on Test.py

--- DRF/api/tests.py
+++ DRF/api/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from api.models import Order, User
 from django.urls import reverse
+from rest_framework import status
 
 # Create your tests here.
 class UserOrderTestCase(TestCase):
@@ -17,6 +18,10 @@ class UserOrderTestCase(TestCase):
         self.client.force_login(user)
         response = self.client.get(reverse('user-orders'))
 
-        assert response.status_code == 200
+        assert response.status_code == status.HTTP_200_OK
         orders = response.json()
         self.assertTrue(all(order['user'] == user.id for order in Orders)) # we want the each order in the respone has true for the specific user id
+
+    def test_user_order_list_unauthenticated(self):
+        response = self.client.get(reverse('user-orders'))
+        self.assertEqual(respone.status_code, status.HTTP_403_FORBIDDEN)
